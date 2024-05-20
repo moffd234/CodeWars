afd849eff30cb8f0f514f38e4fc61ae6ed90b0f7
moffd234 <moffd234@gmail.com>
2024-05-20 13:23:31 -0400

    Created skeleton of the project

---
+++ Build Tower/src/main/java/org/example/Kata.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Kata
+{
+    public static String[] towerBuilder(int nFloors)
+    {
+        return new String[0];
+    }
+}
---
+++ Build Tower/src/test/java/SolutionTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.example.Kata;
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void basicTests() {
+        assertEquals(String.join(",", "*"),
+                String.join(",", Kata.towerBuilder(1)));
+
+        assertEquals(String.join(",", " * ", "***"),
+                String.join(",", Kata.towerBuilder(2)));
+
+        assertEquals(String.join(",", "  *  ", " *** ", "*****"),
+                String.join(",", Kata.towerBuilder(3)));
+    }
+}
