e0cc528d5f3a6369d5cd943965966baccee5de0b
moffd234 <moffd234@gmail.com>
2024-05-22 13:17:42 -0400

    Made more tests for the pc.check()

---
+++ Detect-Pangram/src/main/java/org/example/PangramChecker.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class PangramChecker {
+    public boolean check(String sentence){
+
+        return false;
+    }
+}
---
+++ Detect-Pangram/src/test/java/pangramTest.java
@@ -0,0 +1,68 @@
+import com.sun.source.util.ParameterNameProvider;
+import org.example.PangramChecker;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class pangramTest {
+    PangramChecker pc;
+
+    @Before
+    public void setUp(){
+        pc = new PangramChecker();
+    }
+
+    @Test
+    public void test1() {
+        String pangram1 = "The quick brown fox jumps over the lazy dog";
+        assertTrue(pc.check(pangram1));
+    }
+    @Test
+    public void test2() {
+        String pangram2 = "You shall not pass";
+        assertFalse(pc.check(pangram2));
+    }
+
+    @Test
+    public void testNumbers() {
+        String pangram = "The quick2 brown fox3 jum56ps over the lazy dog";
+        assertTrue(pc.check(pangram));
+    }
+
+    @Test
+    public void testNumbersFalse() {
+        String pangram = "The quick2 brown fox jumps over the laz";
+        assertFalse(pc.check(pangram));
+    }
+
+    @Test
+    public void testSpecial() {
+        String pangram = "The quic$k b&row^n f*ox ju!mps over the lazy dog";
+        assertTrue(pc.check(pangram));
+    }
+
+    @Test
+    public void testSpecialFalse() {
+        String pangram = "The quic$k b&row^n f*ox ju!mps over";
+        assertFalse(pc.check(pangram));
+    }
+
+    @Test
+    public void testSpecialAndNumbers() {
+        String pangram = "1The qu!ick brow7n fo&x ju^7mps o*7ver th89e lazy !dog.!";
+        assertTrue(pc.check(pangram));
+    }
+
+    @Test
+    public void testSpecialAndNumbersFalse() {
+        String pangram = "1The qu!ick brow7n fo&x ju^7mps o*7ver th89e.!";
+        assertTrue(pc.check(pangram));
+    }
+
+    @Test
+    public void testEmpty() {
+        String pangram = "";
+        assertFalse(pc.check(pangram));
+    }
+}
