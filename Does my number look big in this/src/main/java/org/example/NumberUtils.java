928ef72f60696dd5bdcdc6b38b643f35a5a83d34
moffd234 <moffd234@gmail.com>
2024-05-27 12:40:52 -0400

    Created tests for getSum

--- Does my number look big in this/src/main/java/org/example/NumberUtils.java
+++ Does my number look big in this/src/main/java/org/example/NumberUtils.java
@@ -3,8 +3,11 @@ package org.example;
 public class NumberUtils {
 
     public static boolean isNarcissistic(int number) {
-        // TODO replace with your code
         return false;
+    }
+
+    public static int getSum(int n){
+        return 0;
     }
 
 }
---
+++ Does my number look big in this/src/test/java/getSumTests.java
@@ -0,0 +1,35 @@
+import org.example.NumberUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class getSumTests {
+
+    @Test
+    public void test0() {
+        int expected = 0;
+        int actual = NumberUtils.getSum(0);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test100() {
+        int expected = 1;
+        int actual = NumberUtils.getSum(100);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test1000() {
+
+        int expected = 1;
+        int actual = NumberUtils.getSum(1000);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test12345() {
+        int expected = 15;
+        int actual = NumberUtils.getSum(12345);
+        Assert.assertEquals(expected, actual);
+    }
+}
