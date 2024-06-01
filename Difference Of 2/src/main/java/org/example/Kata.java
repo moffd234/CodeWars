f64e121743863cac8cd86c4e6871bb983cdcb738
moffd234 <moffd234@gmail.com>
2024-06-01 12:16:59 -0400

    Finished subArray method

--- Difference Of 2/src/main/java/org/example/Kata.java
+++ Difference Of 2/src/main/java/org/example/Kata.java
@@ -8,6 +8,22 @@ public class Kata {
     }
 
     public static int[][] findSubArrays(int[] input) {
-        return null;
+        int n = input.length;
+        if (n < 2) {
+            return new int[][]{};
+        }
+
+        int[][] subArrays = new int[(n * (n - 1)) / 2][2];
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                subArrays[index][0] = input[i];
+                subArrays[index][1] = input[j];
+                index++;
+            }
+        }
+
+        return subArrays;
     }
 }
