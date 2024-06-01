13906944056aa8c9294bd710d1507297b1960216
moffd234 <moffd234@gmail.com>
2024-06-01 12:18:36 -0400

    Added an extra test for findSubArray

--- Difference Of 2/src/test/java/SubArrayTests.java
+++ Difference Of 2/src/test/java/SubArrayTests.java
@@ -18,6 +18,18 @@ public class SubArrayTests {
     }
 
     @Test
+    public void findSubArray1() {
+        int[] input = new int[]{1, 2, 3, 4};
+        int[][] expected = new int[][]
+                {{1, 2}, {1, 3}, {1, 4},
+                        {2, 3}, {2, 4},
+                        {3, 4}};
+
+        int[][] actual = Kata.findSubArrays(input);
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
     public void findSubArrayEmpty() {
         int[] input = new int[]{};
         int[][] expected = new int[][]{};
