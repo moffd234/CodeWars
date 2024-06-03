201be911e8f8132f565cf2c9ca51d1c323ce9bf9
moffd234 <moffd234@gmail.com>
2024-06-03 16:44:33 -0400

    Finished getMap and FindIt

--- Find Odd Int/src/main/java/org/example/FindOdd.java
+++ Find Odd Int/src/main/java/org/example/FindOdd.java
@@ -1,7 +1,24 @@
 package org.example;
 
+
+import java.util.HashMap;
+
 public class FindOdd {
     public static int findIt(int[] a) {
+        HashMap<Integer, Integer> map = getMap(a);
+        for(HashMap.Entry<Integer, Integer> entry : map.entrySet()){
+            if(entry.getValue() % 2 != 0){
+                return entry.getKey();
+            }
+        }
         return 0;
+    }
+
+    public static HashMap<Integer, Integer> getMap(int[] a) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : a) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        return map;
     }
 }
