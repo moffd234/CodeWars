acc5a08cddfc67dafb4ecee8d73bd97013b82bcc
moffd234 <159962112+moffd234@users.noreply.github.com>
2024-12-09 22:09:16 -0500

    Wrote solution

--- LeetCode/Two Sum/src/main/java/org/example/Solution.java
+++ LeetCode/Two Sum/src/main/java/org/example/Solution.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numIndices = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            numIndices.put(nums[i], i);
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(numIndices.containsKey(target - nums[i]) && numIndices.get(target - nums[i]) != i){
+                return new int[]{i, numIndices.get(target - nums[i])};
+            }
+        }
         return null;
     }
 }
