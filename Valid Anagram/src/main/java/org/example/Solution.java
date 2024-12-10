805bb7208ef22930eee613eebcdc8ae9e4a40a6f
moffd234 <159962112+moffd234@users.noreply.github.com>
2024-12-09 20:50:21 -0500

    Created project and tests

---
+++ Valid Anagram/src/main/java/org/example/Solution.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        return false;
+    }
+}
---
+++ Valid Anagram/src/test/java/isAnagramTest.java
@@ -0,0 +1,33 @@
+import org.example.Solution;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class isAnagramTest {
+    Solution solution;
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
+    @Test
+    public void testIsAnagramTrue() {
+        String s = "racecar";
+        String t = "carrace";
+
+        boolean actual = solution.isAnagram(s, t);
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void testIsAnagramFalse() {
+        String s = "jar";
+        String t = "jam";
+
+        boolean actual = solution.isAnagram(s, t);
+
+        Assert.assertFalse(actual);
+    }
+}
